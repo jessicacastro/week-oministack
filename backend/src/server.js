95a8e0ae2d82b5928aed1fbf323afadfcdd1c575
Jessica Castro <jessica.castros@outlook.com>
2019-08-08 02:17:32 -0300

    Importando o mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,22 @@
  **/
 const express = require("express");
 
+// Importando o mongoose para utilizar sintaxe javascript na manipulação de banco.
+const mongoose = require('mongoose');
+
 // Importanto arquivo de rotas.
 const routes = require('./routes');
 
 // Criando um servidor do express.
 const server = express();
 
+// Fazendo a conexão com o banco de dados.
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-21gmd.mongodb.net/omnistack?retryWrites=true&w=majority', { useNewUrlParser : true})
+
+// Indicando ao express que a aplicação back irá receber requisições com JSON.
+server.use(express.json());
+
+// Colocando a configuração de rotas para ser usada pelo server
 server.use(routes);
 
 server.listen(3333); // PORTA QUE O SERVIDOR ESTÁ OUVINDO
